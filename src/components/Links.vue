a943f0ef07770857c4b1d9337e099ba2eac66bde
Wangcy <93115719+wchenyi@users.noreply.github.com>
2024-07-11 00:13:37 +0800

    Update Links.vue

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, IosSchool, CalendarPerson16Filled, Search, Wallpaper, GiftCardMoney24Filled } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -64,12 +64,11 @@ const siteLinksList = computed(() => {
 // 网站链接图标
 const siteIcon = {
   Blog,
-  Cloud,
-  CompactDisc,
-  Compass,
-  Book,
-  Fire,
-  LaptopCode,
+  IosSchool,
+  CalendarPerson16Filled,
+  Search,
+  Wallpaper,
+  GiftCardMoney24Filled,
 };
 
 // 链接跳转
