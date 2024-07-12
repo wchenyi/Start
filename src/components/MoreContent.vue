98ef03fae5339a60a211d7dbecac1b6e35717f2c
Wangcy <93115719+wchenyi@users.noreply.github.com>
2024-07-12 12:16:35 +0800

    Update MoreContent.vue

--- src/components/MoreContent.vue
+++ src/components/MoreContent.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="more-content">您可在此编写任意内容</div>
+  <div class="more-content">
+    亲爱的来访者你好啊👏<br>
+        欢迎来到本站😊<br>
+        本站是作者“Wangcy”所制作的各种站点的汇总点<br>
+        不管你是否是无意间来到本站，都希望你能生活愉快、万事如意！<br>
+        这里有我脑子一抽写的项目，也有描述自己内心风景的博客<br>
+        衷心希望来到这里的你能够找到你需要的东西🎉<br>
+                                              Author:Wangcy
+  </div>
 </template>
 
 <style lang="scss" scoped>
